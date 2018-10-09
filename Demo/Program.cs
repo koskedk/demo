42563603ded5c9105b28ee3e5b1b09584c1605f0
koske kimutai <koskedk@live.com>
2018-10-09 15:33:03 +0300

    initial commit

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Demo
 {
@@ -14,11 +12,45 @@ namespace Demo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(LogEventLevel.Debug)
+                .WriteTo.RollingFile("logs/{Date}-error.log", LogEventLevel.Error)
+                .CreateLogger();
+            
+            try
+            {
+                Log.Information("Starting demo...");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("hosting.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+            
+            return host;
+        }
     }
 }
--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -1,31 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using Demo.Core.Commands;
+using Demo.Infrastructure;
+using Hangfire;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Serilog;
 
 namespace Demo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public static IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMediatR(typeof(SaveFacility));
+            
+            var connectionString = Startup.Configuration["ConnectionStrings:eticaConnection"];
+            
+            services.AddDbContext<DemoContext>(o => o.UseSqlServer(connectionString,
+                x => x.MigrationsAssembly(typeof(DemoContext).GetTypeInfo().Assembly.GetName().Name)));
+
+            try
+            {
+                services.AddHangfire(o => o.UseSqlServerStorage(connectionString));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Startup Error on HangFire");
+            }
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +65,18 @@ namespace Demo
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            
+            try
+            {
+                app.UseHangfireDashboard("/api/hangfire");
+                app.UseHangfireServer();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Hangfire is down !");
+            }
+            
+            Log.Debug("Startup OK");
         }
     }
 }
